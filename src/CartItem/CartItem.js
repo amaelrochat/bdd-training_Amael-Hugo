dc79057a8a5052123903ec83553a42b9c412ac77
amaeljampen <amael.jampen@eduvaud.ch>
2025-11-25 13:01:26 +0100

    fix: setter and attribute name

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -8,7 +8,7 @@ module.exports = class CartItem {
 
     //region private attributes
     #articleId;
-    #name;
+    #nameValue;
     #quantity;
     #price;
     //endregion private attributes
@@ -18,7 +18,7 @@ module.exports = class CartItem {
         this.#articleIdSetter(articleId);
         this.quantity(quantity);
         this.price(price);
-        this.#nameSetter(name);
+        this.#name(name);
     }   
 
 
@@ -59,8 +59,8 @@ module.exports = class CartItem {
         return this.#articleId = value;
     }
 
-    set #nameSetter(value) {
-        return this.#price = value;
+    set #name(value) {
+        return this.#nameValue = value;
     }
 
     #validateArticleId(articleId) {
